ab9f0327e8c0f84ca1e722e775aff5f518376df9
Alex Milewski <alex@mechies.com>
2025-03-29 16:10:51 -0500

    Update codaConfig.js

--- codaConfig.js
+++ codaConfig.js
@@ -1,4 +1,5 @@
-// Coda credentials and table config
+// Coda + Google Maps credentials and table setup
 const CODA_API_TOKEN = 'fd33c17c-f859-49aa-9007-de437a4d642f';
 const DOC_ID = 'yU8ag5Yx9I';
 const TABLE_ID = 'grid-oRzNlTd38V';
+const GOOGLE_MAPS_API_KEY = 'AIzaSyBBfP6bL3mmcznLSgj7pYX8t5E5lbiOt38';
