5de1e32735f54a7db7b4c0e0097660bfde9edd52
Alex Milewski <alex@mechies.com>
2025-03-30 14:14:49 -0500

    Update codaConfig.js

--- codaConfig.js
+++ codaConfig.js
@@ -1,4 +1,1 @@
-window.CODA_API_TOKEN = 'fd33c17c-f859-49aa-9007-de437a4d642f';
-window.DOC_ID = 'yU8ag5Yx9I';
-window.TABLE_ID = 'grid-oRzNlTd38V';
-window.GOOGLE_MAPS_API_KEY = 'AIzaSyBBfP6bL3mmcznLSgj7pYX8t5E5lbiOt38';
+
